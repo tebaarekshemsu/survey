a3b797315f97ab0dab449cfb19de5be39958d7df
tebarek <tebarek29@gmail.com>
2025-08-13 14:46:36 +0300

    added database scheme

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
-import { admin } from "better-auth/plugins"
+import { PrismaClient } from "../../generated/prisma";
+import { admin } from "better-auth/plugins";
+import { sendEmail } from "../common/email";
 
 const prisma = new PrismaClient();
 
@@ -12,7 +13,15 @@ export const auth: ReturnType<typeof betterAuth> = betterAuth({
   
   emailAndPassword: {
     enabled: true, 
-  }, 
+  },  emailVerification: {
+    sendVerificationEmail: async ( { user, url, token }, request) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Verify your email address",
+        text: `Click the link to verify your email: ${url}`,
+      });
+    },
+  },
   plugins: [
         admin() 
     ]
